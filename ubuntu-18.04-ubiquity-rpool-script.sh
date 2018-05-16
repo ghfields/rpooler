3f920082303c2a1812889ac1c727816b68b1f5a3
Garrett Fields <ghfields@gmail.com>
2018-05-16 16:12:13 -0400

    Update ubuntu-18.04-ubiquity-rpool-script.sh

--- ubuntu-18.04-ubiquity-rpool-script.sh
+++ ubuntu-18.04-ubiquity-rpool-script.sh
@@ -14,6 +14,20 @@ for i in $(ls /dev/disk/by-id/ -a |grep -v part |awk '{if(NR>2)print}');do echo 
 read -p "What vdev layout do you want to use? (hint: tab completion works): " -e LAYOUT
 echo ""
 read -p "Which zpool & zfs options do you wish to set at creation? " -i "-o ashift=12 -O atime=off -O compression=lz4 -O normalization=formD -O recordsize=1M -O xattr=sa" -e OPTIONS
+
+while true; do
+    read -p "Ubiquity made swapfile is removed.  Want to use a swap zvol?" yn
+    case $yn in
+        [Yy]* ) SWAPZVOL=4; break;;
+        [Nn]* ) SWAPZVOL=0; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+
+
+
 read -p "Provide an IP of a nameserver available on your network: " -i "8.8.8.8" -e NAMESERVER
 DRIVES="$(echo $LAYOUT | sed 's/\S*\(mirror\|raidz\|log\|spare\|cache\)\S*//g')"
 
@@ -38,6 +52,11 @@ ubiquity --no-bootloader
 
 zfs create $POOL/ROOT
 zfs create $POOL/ROOT/ubuntu-1
+
+if [[ $SWAPZVOL -ne 0 ]]; then
+fi
+
+
 rsync -avPX /target/. /$POOL/ROOT/ubuntu-1/.
 
 for d in proc sys dev; do mount --bind /$d /$POOL/ROOT/ubuntu-1/$d; done
@@ -49,6 +68,15 @@ echo "nameserver " $NAMESERVER | tee -a /$POOL/ROOT/ubuntu-1/etc/resolv.conf
 sed -e '/\s\/\s/ s/^#*/#/' -i /$POOL/ROOT/ubuntu-1/etc/fstab  #My take at comment out / line.
 sed -e '/\sswap\s/ s/^#*/#/' -i /$POOL/ROOT/ubuntu-1/etc/fstab #My take at comment out swap line.
 
+if [[ $SWAPZVOL -ne 0 ]]; then
+     zfs create create -V $SWAPZVOL -b $(getconf PAGESIZE) -o compression=zle \
+      -o logbias=throughput -o sync=always \
+      -o primarycache=metadata -o secondarycache=none \
+      -o com.sun:auto-snapshot=false $POOL/swap
+     mkswap -f /dev/zvol/$POOL/swap
+     echo RESUME=none > /$POOL/ROOT/ubuntu-1/etc/initramfs-tools/conf.d/resume
+     echo /dev/zvol/rpool/swap none swap defaults 0 0 >> /$POOL/ROOT/ubuntu-1/etc/fstab
+fi
 
 chroot /$POOL/ROOT/ubuntu-1 apt update
 chroot /$POOL/ROOT/ubuntu-1 apt install -y zfs-initramfs
